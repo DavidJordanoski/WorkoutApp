191071097e4a0e327927630658ef7708136a2731
Deksio99 <jordanoskidavid@gmail.com>
2022-06-12 18:05:53 +0200

    Firebase Added

--- app/src/main/java/com/example/workoutapp/RegisterActivity.java
+++ app/src/main/java/com/example/workoutapp/RegisterActivity.java
@@ -1,10 +1,14 @@
 package com.example.workoutapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,22 +18,38 @@ import android.widget.Toast;
 import com.example.workoutapp.roomdatabase.User;
 import com.example.workoutapp.roomdatabase.UserDAO;
 import com.example.workoutapp.roomdatabase.UserDataBase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText editTextUsername, editTextEmail, editTextPassword, editTextConfirmPassword;
     Button buttonRegister;
     TextView textViewLogin;
     private UserDAO userDAO;
+    FirebaseFirestore fireStore;
+    FirebaseAuth fAuth;
+    String userName, email, password, passwordConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fireStore = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextConfirmPassword = findViewById(R.id.editTextCnfPassword);
+
+
 
         buttonRegister = findViewById(R.id.buttonRegister);
 
@@ -37,31 +57,100 @@ public class RegisterActivity extends AppCompatActivity {
         textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             }
         });
 
-        userDAO = Room.databaseBuilder(this, UserDataBase.class,"User")
+        userDAO = Room.databaseBuilder(this, UserDataBase.class, "User")
                 .allowMainThreadQueries()
                 .build().getUserDao();
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                editTextUsername = findViewById(R.id.editTextUsername);
+                editTextEmail = findViewById(R.id.editTextEmail);
+                editTextPassword = findViewById(R.id.editTextPassword);
+                editTextConfirmPassword = findViewById(R.id.editTextCnfPassword);
+
                 String userName = editTextUsername.getText().toString().trim();
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
                 String passwordConfirm = editTextConfirmPassword.getText().toString().trim();
+                fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("username", userName);
+                        user.put("email", email);
+                        user.put("password", password);
+                        user.put("confirmPassword", passwordConfirm);
 
-                if (password.equals(passwordConfirm)){
-                    User user = new User(userName,password,email);
-                    userDAO.insert(user);
-                    Intent moveToLogin = new Intent(RegisterActivity.this,LoginActivity.class);
-                    startActivity(moveToLogin);
-                } else {
-                    Toast.makeText(RegisterActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
-                }
+                        fireStore.collection("users")
+                                .add(user)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Toast.makeText(RegisterActivity.this, "Data stored", Toast.LENGTH_SHORT);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(RegisterActivity.this, "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
             }
         });
+    }
+
+    private void fireStoreDataBase() {
+        String userName = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String passwordConfirm = editTextConfirmPassword.getText().toString().trim();
+        fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Map<String, Object> user = new HashMap<>();
+                user.put("username", userName);
+                user.put("email", email);
+                user.put("password", password);
+                user.put("confirmPassword", passwordConfirm);
+
+                fireStore.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(RegisterActivity.this, "Data stored", Toast.LENGTH_SHORT);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity.this, "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+    }
+
+    private void roomDatabase() {
+        String userName = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String passwordConfirm = editTextConfirmPassword.getText().toString().trim();
+
+        if (password.equals(passwordConfirm)) {
+            User user = new User(userName, password, email);
+            userDAO.insert(user);
+            Intent moveToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(moveToLogin);
+        } else {
+            Toast.makeText(RegisterActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
+        }
     }
 }
